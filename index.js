870a48003d0acd52c4b99c7c35c616ed1d953c43
Jaime Pineda <jaime.pineda.hqz@gmail.com>
2021-10-27 14:08:14 -0600

    🧪 test simple console log

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-'use strict';
+// 'use strict';
 
-const mongoose = require('mongoose');
-const app = require('./app');
-const port = 3700;
+// const mongoose = require('mongoose');
+// const app = require('./app');
+// const port = 3700;
 
-main()
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('Successfully connected to the database');
+// main()
+//   .then(() => {
+//     // eslint-disable-next-line no-console
+//     console.log('Successfully connected to the database');
 
-    app.listen(port, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Server is running: http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    // eslint-disable-next-line no-console
-    console.error(err);
-  });
+//     app.listen(port, () => {
+//       // eslint-disable-next-line no-console
+//       console.log(`Server is running: http://localhost:${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     // eslint-disable-next-line no-console
+//     console.error(err);
+//   });
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/portfolio');
-}
+// async function main() {
+//   await mongoose.connect('mongodb://localhost:27017/portfolio');
+// }
+
+console.log('hello world');
