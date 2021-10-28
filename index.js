de242b6a9be44bf4a1064f5ee800673ee6222272
Jaime Pineda <jaime.pineda.hqz@gmail.com>
2021-10-28 11:11:16 -0600

    ✨ use env variables for db credentials

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+require('dotenv').config();
+
 main()
   .then(() => {
     // eslint-disable-next-line no-console
@@ -11,7 +13,7 @@ main()
 
     app.listen(port, () => {
       // eslint-disable-next-line no-console
-      console.log(`Server is running: http://localhost:${port}`);
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((err) => {
@@ -21,6 +23,6 @@ main()
 
 async function main() {
   await mongoose.connect(
-    'mongodb+srv://jpin730:PVmhD&n28eu^@cluster0.j5cov.mongodb.net/portfolio'
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j5cov.mongodb.net/portfolio`
   );
 }
